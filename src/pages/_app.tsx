0221a463736c730f8c86b52c1c2033706ae75f58
Jorge Raul Ochoa-Reyes <jorgeochoareyes@gmail.com>
2025-01-19 17:53:03 -0800

    redirect to sign in

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,31 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
 import { GeistSans } from "geist/font/sans";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { SidebarProvider, SidebarInset } from "~/components/ui/sidebar";
 import { AppSidebar } from "~/components/app-sidebar"; 
-import { ThemeProvider } from "~/components/theme-provider";
+import { ThemeProvider } from "~/components/theme-provider"; 
+import { useRouter } from "next/router";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { useEffect } from "react";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+  const router = useRouter();
+  
+  // redirect to login if not authenticated
+  useEffect(() => {
+    if (!session && router.pathname !== "/sign-in") {
+      router.push("/sign-in");
+    }
+  }, [session]);
+
   return (
     <SessionProvider session={session}>
       <ThemeProvider
@@ -21,15 +33,22 @@ const MyApp: AppType<{ session: Session | null }> = ({
         defaultTheme="system"
         enableSystem
         disableTransitionOnChange
-      >
-        <SidebarProvider>
-          <AppSidebar />
-          <SidebarInset>
+      > 
+        {    
+          !session ? 
             <div className={GeistSans.className}>
               <Component {...pageProps} />
             </div>
-          </SidebarInset>
-        </SidebarProvider>
+            :
+            <SidebarProvider>
+              <AppSidebar />
+              <SidebarInset>
+                <div className={GeistSans.className}>
+                  <Component {...pageProps} />
+                </div>
+              </SidebarInset>
+            </SidebarProvider>
+        }
       </ThemeProvider>
     </SessionProvider>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,78 +1,149 @@
-import { signIn, signOut, useSession } from "next-auth/react";
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "~/components/ui/breadcrumb";
-import { Separator } from "~/components/ui/separator";
-import { SidebarTrigger } from "~/components/ui/sidebar";
+import React, { type ReactNode, useRef } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { FiArrowUpRight } from "react-icons/fi";
 
-import { api } from "~/utils/api";
-
-export default function Home() {  
-  const asd = api.post.getSecretMessage.useQuery(undefined, { enabled: true });
+export default function TextParallaxContentExample(){
   return (
-    <>
-      <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-        <div className="flex items-center gap-2 px-4"> 
-          <SidebarTrigger className="-ml-1" />
-          <Separator orientation="vertical" className="mr-2 h-4" />
-          <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbItem className="hidden md:block">
-                <BreadcrumbLink href="#">
-                    Building Your Application
-                </BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator className="hidden md:block" />
-              <BreadcrumbItem>
-                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb> 
-        </div>
-      </header>
-      <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-        <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-          <div className="aspect-video rounded-xl bg-muted/50" />
-          <div className="aspect-video rounded-xl bg-muted/50" />
-          <div className="aspect-video rounded-xl bg-muted/50" />
-        </div>
-        <AuthShowcase />
-        <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
-          {
-            asd.isLoading ? (
-              <p>Loading...</p>
-            ) : asd.isError ? (
-              <p> Error: {asd.error.message}</p>
-            ) : asd.isSuccess ? (
-              asd.data
-            ) : (
-              "Error"
-            )
-          }
-        </div>
-      </div>
-    </>
-  );
-}
-
-function AuthShowcase() {
-  const { data: sessionData } = useSession();
-
-  const { data: secretMessage } = api.post.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined }
-  );
-
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
-      </p>
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => void signOut() : () => void signIn()}
+    <div className="bg-white">
+      <TextParallaxContent
+        imgUrl="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        subheading="Collaborate"
+        heading="Built for all of us."
       >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
+        <ExampleContent />
+      </TextParallaxContent>
+      <TextParallaxContent
+        imgUrl="https://images.unsplash.com/photo-1530893609608-32a9af3aa95c?q=80&w=2564&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        subheading="Quality"
+        heading="Never compromise."
+      >
+        <ExampleContent />
+      </TextParallaxContent>
+      <TextParallaxContent
+        imgUrl="https://images.unsplash.com/photo-1504610926078-a1611febcad3?q=80&w=2416&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        subheading="Modern"
+        heading="Dress for the best."
+      >
+        <ExampleContent />
+      </TextParallaxContent>
     </div>
   );
-}
+};
+
+const IMG_PADDING = 12;
+
+const TextParallaxContent = ({
+  imgUrl,
+  subheading,
+  heading,
+  children,
+}: {
+  imgUrl: string;
+  subheading: string;
+  heading: string;
+  children: ReactNode;
+}) => {
+  return (
+    <div
+      style={{
+        paddingLeft: IMG_PADDING,
+        paddingRight: IMG_PADDING,
+      }}
+    >
+      <div className="relative h-[150vh]">
+        <StickyImage imgUrl={imgUrl} />
+        <OverlayCopy heading={heading} subheading={subheading} />
+      </div>
+      {children}
+    </div>
+  );
+};
+
+const StickyImage = ({ imgUrl }: { imgUrl: string }) => {
+  const targetRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+    offset: ["end end", "end start"],
+  });
+
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 0.85]);
+  const opacity = useTransform(scrollYProgress, [0, 1], [1, 0]);
+
+  return (
+    <motion.div
+      style={{
+        backgroundImage: `url(${imgUrl})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height: `calc(100vh - ${IMG_PADDING * 2}px)`,
+        top: IMG_PADDING,
+        scale,
+      }}
+      ref={targetRef}
+      className="sticky z-0 overflow-hidden rounded-3xl"
+    >
+      <motion.div
+        className="absolute inset-0 bg-neutral-950/70"
+        style={{
+          opacity,
+        }}
+      />
+    </motion.div>
+  );
+};
+
+const OverlayCopy = ({
+  subheading,
+  heading,
+}: {
+  subheading: string;
+  heading: string;
+}) => {
+  const targetRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+    offset: ["start end", "end start"],
+  });
+
+  const y = useTransform(scrollYProgress, [0, 1], [250, -250]);
+  const opacity = useTransform(scrollYProgress, [0.25, 0.5, 0.75], [0, 1, 0]);
+
+  return (
+    <motion.div
+      style={{
+        y,
+        opacity,
+      }}
+      ref={targetRef}
+      className="absolute left-0 top-0 flex h-screen w-full flex-col items-center justify-center text-white"
+    >
+      <p className="mb-2 text-center text-xl md:mb-4 md:text-3xl">
+        {subheading}
+      </p>
+      <p className="text-center text-4xl font-bold md:text-7xl">{heading}</p>
+    </motion.div>
+  );
+};
+
+const ExampleContent = () => (
+  <div className="mx-auto grid max-w-5xl grid-cols-1 gap-8 px-4 pb-24 pt-12 md:grid-cols-12">
+    <h2 className="col-span-1 text-3xl font-bold md:col-span-4">
+      Additional content explaining the above card here
+    </h2>
+    <div className="col-span-1 md:col-span-8">
+      <p className="mb-4 text-xl text-neutral-600 md:text-2xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi,
+        blanditiis soluta eius quam modi aliquam quaerat odit deleniti minima
+        maiores voluptate est ut saepe accusantium maxime doloremque nulla
+        consectetur possimus.
+      </p>
+      <p className="mb-8 text-xl text-neutral-600 md:text-2xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+        reiciendis blanditiis aliquam aut fugit sint.
+      </p>
+      <button className="w-full rounded bg-neutral-900 px-9 py-4 text-xl text-white transition-colors hover:bg-neutral-700 md:w-fit">
+        Learn more <FiArrowUpRight className="inline" />
+      </button>
+    </div>
+  </div>
+);
