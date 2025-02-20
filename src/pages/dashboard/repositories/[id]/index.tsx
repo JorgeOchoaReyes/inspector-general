54fdc373f9d3393d6855a2a7be3278a62eac3bfd
Jorge Raul Ochoa-Reyes <jorgeochoareyes@gmail.com>
2025-02-19 16:32:30 -0800

    ui and initial chat pr

--- src/pages/dashboard/repositories/[id]/index.tsx
+++ src/pages/dashboard/repositories/[id]/index.tsx
@@ -35,12 +35,12 @@ export default function Home(){
                       <h1 className="text-4xl font-semibold mb-4">
                         {repoDetails.data?.full_name.split("/")[1]}
                         <br /> 
-                        <div className="font-normal text-sm text-gray-500 flex-wrap flex max-w-[70%] mt-2">
-                         Repo Description: {repoDetails.data?.description === "" ?  "N/A" : repoDetails.data?.description}
+                        <div className="font-normal text-sm text-gray-500 flex-wrap flex mt-2">
+                          Repo Description: {repoDetails.data?.description === "" ?  "N/A" : repoDetails.data?.description} 
                         </div>
                       </h1> 
                     </div> 
-                    <div>
+                    <div className="h-full">
                       <div className="grid auto-rows-min gap-4 md:grid-cols-3">
                         <div className="aspect-video text-2xl rounded-xl flex-col bg-muted/50 flex items-center self-start justify-center p-4 gap-5">
                           <h3 className="text-2xl font-semibold justify-start">
@@ -72,11 +72,7 @@ export default function Home(){
                             onClick={async () => await navigator.clipboard.writeText(repoDetails?.data?.clone_url ?? "")}>
                             {repoDetails.data?.private ? <Lock /> : <LockKeyholeOpenIcon />}   
                             {repoDetails.data?.private ? "Private" : "Public"}
-                          </div>
-                          <div className="flex text-sm flex-row items-start self-start gap-2" >
-                            <MemoryStick /> 
-                            {repoDetails.data?.size} mb
-                          </div>
+                          </div> 
                         </div>
                         <div className="aspect-video rounded-xl bg-muted/50 flex flex-col items-center justify-center">
                           <h3 className="text-2xl font-semibold gap-4 flex flex-row">
@@ -105,8 +101,8 @@ export default function Home(){
                           </div>
                         </div>
                       </div>  
-                      <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min aspect-video mt-5"> 
-                        <div className="flex flex-row justify-between items-center gap-4 p-6 ">  
+                      <div className="flex-1 rounded-xl h-fit bg-muted/50 aspect-video mt-5"> 
+                        <div className="flex flex-row justify-between items-center gap-4 p-6">  
                           <h2 className="text-2xl font-semibold flex flex-row items-center gap-4"> 
                             <GitPullRequest /> Pull Request 
                           </h2>
@@ -120,7 +116,7 @@ export default function Home(){
                             ]}
                           />
                         </div> 
-                        <div className="flex flex-col gap-4 p-4 pt-0"> 
+                        <div className="flex flex-col gap-4 p-4 pt-0 h-full"> 
                           {
                             pullRequest.isPending ? (
                               <div className="flex flex-col gap-4 rounded-xl p-4 items-center justify-center"> 
@@ -194,7 +190,7 @@ export default function Home(){
                           } 
                         </div> 
                       </div>
-                    </div>
+                    </div> 
                   </FadeInSlide>
               }
             </div>    
